86a3d344205304a5c892c41dbdb6c799750a8c5e
brobat6 <ldrubra8@gmail.com>
2022-11-13 14:34:31 +0530

    Added Euclidean Algo, Diophantine equation

---
+++ number-theory/1A_gcd_fast.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Binary GCD (faster)
+// Source : https://cp-algorithms.web.app/algebra/euclid-algorithm.html#binary-gcd
+int gcd(int a, int b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+            swap(a, b);
+        b -= a;
+    } while (b);
+    return a << shift;
+}
+
+// iterative Euclidean algorithm for finding GCD
+int gcd(int a, int b, int& x, int& y) {
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+        int q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+    }
+    return a1;
+}
