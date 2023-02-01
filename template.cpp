74e5442b8a00de43fa1720d4dc268936f3a97527
brobat6 <ldrubra8@gmail.com>
2023-02-01 08:49:22 +0530

    Added template (untested, unreviewed)

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+// #define double long double
+#define rep(i, a, b) for(int i = a; i < (b); i++)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vector<int>> vvi;
+
+const int N = 200100;
+const int MOD = 998244353;
+// const int MOD = (int)1E9 + 7;
+const double EPS = 1E-9;
+
+void solve() {
+    
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout << setprecision(20) << fixed;
+
+    int T = 1;
+    cin >> T;
+    for(int I = 1; I <= T; I++) {
+        // cout << "Case #" << I << ": ";
+        solve();
+    }
+
+    return 0;
+}
